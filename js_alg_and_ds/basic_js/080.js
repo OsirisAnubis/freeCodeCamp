d3fa27b7a3fe15eecc3a92eb12cbf6766d83f207
OsirisAnubis <olegshulgakalendar@gmail.com>
2022-07-31 16:27:26 +0300

    add solutions

---
+++ js_alg_and_ds/basic_js/080.js
@@ -0,0 +1,11 @@
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if ((10 > val) || (val > 20)) {
+    return "Outside";
+  }
+  // Only change code above this line
+  return "Inside";
+}
+
+testLogicalOr(15);
